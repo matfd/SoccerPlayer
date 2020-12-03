fca5da05fc6242a24f6a6648358a42cdeea88009
matfd <S2a0s0h5a01122005@gmail.com>
2020-12-03 17:41:44 +0300

    Soccer Player

---
+++ SoccerPlayer.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+class SoccerPlayer {
+private:
+    std::string Famil;
+	unsigned int number;
+	unsigned int Goal;
+	unsigned int Assists;
+public:
+	SoccerPlayer();	
+	SoccerPlayer(std::string, unsigned int, unsigned int, unsigned int);
+	void setFamil(std::string fam);
+	std::string getFamil();
+	void setNum(unsigned int fnum);
+	unsigned int getNum();
+	void setGoal(unsigned int fGoal);
+	unsigned int getGoal();
+	void setAssists(unsigned int fAssist);
+	unsigned int getAssists();
+};
